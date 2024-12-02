a4d57c9daa9788f58b3ec1a6d8d1915a6b468d8a
Joao-Oliveira9 <joaoliveira9@outlook.com>
2024-12-02 14:23:46 -0300

    Update: Use bean Service instead Component

--- src/main/java/com/joao/cadastro/core/services/CriarResgistroAlunoService.java
+++ src/main/java/com/joao/cadastro/core/services/CriarResgistroAlunoService.java
@@ -5,8 +5,9 @@ import com.joao.cadastro.core.entities.Aluno;
 import com.joao.cadastro.core.usecases.CriarRegistroAlunoUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CriarResgistroAlunoService implements CriarRegistroAlunoUseCase {
     @Autowired
     AlunoRepository alunoRepository;
